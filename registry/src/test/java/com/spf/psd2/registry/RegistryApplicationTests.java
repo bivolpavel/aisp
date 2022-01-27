6848824912e0d32127951bdb5c7d9af6c22d12b3
bivolpavel <31041255+bivolpavel@users.noreply.github.com>
2022-01-27 02:04:52 +0200

    Add files via upload

---
+++ registry/src/test/java/com/spf/psd2/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spf.psd2.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
